19c86067dc37c2a213e631a98d7023a78fc3d5f8
Anastasia Rambaud <anastasia.rambaud@gmail.com>
2022-01-19 17:27:13 +0100

    Add function to create memes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,141 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [topText, setTopText] = useState('');
+  const [bottomText, setBottomText] = useState('');
+  const [templateUrl, setTemplateUrl] = useState('');
+  const [memeTemplate, SetMemeTemplate] = useState('');
+  const [memeUrl, setMemeUrl] = useState('');
+  //   // Get template list
+  //   function fetchTemplates() {
+  //     return fetch('https://api.memegen.link/templates')
+  //       .then((response) => response.json())
+  //       .then((data) => {
+  //         // Creates array list of url templates
+  //         let templateList = data.map((e) => ({
+  //           name: e.name,
+  //           url: e.blank,
+  //         }
+  //         // Searchs for the input in the arary
+  //         for (let i in templateList ) {
+  //           if (i.name.includes(memeTemplate)) {
+  //             setTemplateUrl(i.url);
+  //             break;
+  //           };
+  //         }
+
+  //         ));
+  //         console.log(templateObject);
+  //       })
+  //       .catch((error) => console.error(error));
+  //   }
+
+  function searchArray(arr) {
+    for (let i = 0; i < arr.length; i++) {
+      // Check if string includes and makes it case insensitive
+      if (arr[i].name.toLowerCase().includes(memeTemplate.toLowerCase())) {
+        setTemplateUrl(arr[i].url);
+        console.log(arr[i].url);
+        break;
+      } else if (i === arr.length - 1) {
+        console.log('No such template');
+      }
+    }
+  }
+
+  // Search for template URL
+  function SearchUrl(event) {
+    SetMemeTemplate(event.currentTarget.value);
+    // Get the templates
+    function fetchTemplates() {
+      return fetch('https://api.memegen.link/templates')
+        .then((response) => response.json())
+        .then((data) => {
+          // Creates array list of url templates
+          const templateList = data.map((e) => ({
+            name: e.name,
+            url: e.blank,
+          }));
+          console.log(templateList);
+          searchArray(templateList);
+        })
+        .catch((error) => console.error(error));
+    }
+    fetchTemplates();
+  }
+
+  function CreateMeme({ templateUrl, topText, bottomText }) {
+    // useEffect(
+    //   (event) => {
+    //     setTopText(event.currentTarget.value);
+    //   },
+    //   [topText],
+    // );
+    // Contains data to create meme
+    const bodyData = {
+      background: templateUrl,
+      style: 'string',
+      text_lines: [topText, bottomText],
+      extension: 'string',
+      redirect: true,
+    };
+
+    // Request meme
+    fetch('https://api.memegen.link/images/custom', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(bodyData),
+    })
+      .then((response) => {
+        const responseUrl = response.url;
+        console.log(responseUrl);
+        setMemeUrl(responseUrl);
+        return response.json();
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <label>
+        Top text
+        <input
+          value={topText}
+          onChange={(event) => {
+            setTopText(event.currentTarget.value);
+            CreateMeme({ templateUrl, topText, bottomText });
+          }}
+        />
+      </label>
+      <label>
+        Bottom text
+        <input
+          value={bottomText}
+          onChange={(event) => {
+            setBottomText(event.currentTarget.value);
+            CreateMeme({ templateUrl, topText, bottomText });
+          }}
+        />
+      </label>
+      <label>
+        Meme template
+        <input
+          value={memeTemplate}
+          onChange={(event) => {
+            SearchUrl(event);
+            CreateMeme({ templateUrl, topText, bottomText });
+          }}
+        />
+      </label>
+      <img src={memeUrl} alt="Meme Template" data-test-id="meme-image" />
+      <a href={memeUrl} download>
+        Click to download
+      </a>
     </div>
   );
 }
